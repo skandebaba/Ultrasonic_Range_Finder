65ac89b571b65ad7c4270f6fe16317d7ca2f9cbe
skandebaba <johann.padkos@gmail.com>
2014-01-13 15:56:00 +1300

    Arduino Code
    Arduino code for HY-SFR05 sensor, currently displaying values in cm, can
    be customized to display inches and cm.

---
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -0,0 +1,56 @@
+/* 
+ *  A simple program that makes use of a HY-SFR05 to find the distance of an object 
+ *  Assuming a room temp of 20 degrees centigrade
+ *
+ *  The circuit:
+ *  VCC connection of the sensor attached to +5V
+ *  GND connection of the sensor attached to GND
+ *  ECHO connection of the sensor attached to 2
+ *  TRIG connection of the sensor attached to 3
+ *
+ *  Datasheet can be found at: http://www.robot-electronics.co.uk/htm/srf05tech.htm
+ */
+ 
+//#define  INCHES    // Uncomment if you want to display inches
+ 
+const char  ECHO_PIN = 2;
+const char  TRIG_PIN = 3;
+
+void setup(){
+  // Start serial communication
+  Serial.begin(9600);
+  
+  // Pin configuration
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop(){
+  float duration, distanceCm, distanceIn;
+  
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN,HIGH);
+  
+  // Convert the time into a distance
+  distanceCm = duration / 29.1 / 2 ;
+  distanceIn = duration / 74 / 2;
+  
+  if (distanceCm <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+#ifndef INCHES
+    Serial.print(distanceIn, 3);
+    Serial.print("in, ");
+#endif
+    Serial.print(distanceCm, 3);
+    Serial.print("cm");
+    Serial.println();
+  }
+  delay(200);
+}
